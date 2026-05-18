cdad02c6c79757b4fae4c140af56fbd549083d4c
statsinthewild <gjm112@gmail.com>
2026-05-18 11:03:09 -0500

    first update.

---
+++ eda.R
@@ -0,0 +1,9 @@
+#Scoring rules: https://okstate.com/news/2026/3/19/cowboy-wrestling-ncaa-championship-team-scoring-breakdown
+#2026 brackets: https://www.ncaa.com/_flysystem/public-s3/files/Final%20Brackets%20wreslting.pdf
+
+#Final team scores: https://www.trackwrestling.com/predefinedtournaments/MainFrame.jsp?newSession=false&TIM=1779119545699&pageName=%2Fpredefinedtournaments%2FPrintTeamScores.jsp&twSessionId=phhwyhangm
+
+#Gregs Rules: The wrestler finishing with rank x + 1, cannot score more than the wrestler who finished x.
+
+#Does losing early and finishing 3rd benefit you?  
+#Think about the case where a wrestler loses first round and then wins 3rd.  
